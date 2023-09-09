2d879d4ec5102b13c42c4f9e0f636a2b41aa7dd7
Fabien Arcellier <fabien.arcellier@gmail.com>
2023-09-09 15:20:28 +0200

    show error output when execution of a command by another executor ends badly
    * fix: display stderr content on sub interpreter

--- tests/fixtures/project_with_venv/alfred/cmd.py
+++ tests/fixtures/project_with_venv/alfred/cmd.py
@@ -4,5 +4,5 @@ import alfred
 @alfred.command('hello')
 def hello():
     cowsay = alfred.sh(['cowsay', 'cowsay.exe'], 'cowsay is missing')
-    alfred.run(cowsay, ["hello"])
+    alfred.run(cowsay, ['-t', "hello"])
 
